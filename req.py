f7fe6e0e6c0e4f932a9a481c0207d99223a44ad4
Oliver <oliverng2512@icloud.com>
2023-05-20 11:19:39 +0100

    Add sus to req.py
    Co-Authored-By: null <ObsidianTurtle@users.noreply.github.com>

--- req.py
+++ req.py
@@ -9,6 +9,11 @@ def main():
         main()
     elif c1 == "v":
         find()
+    elif c1 == "sus":
+        sus += 1
+        print(f"You are X{sus} sus")
+        d["sus"] = sus
+        write("servdata/population.json", d)
     else:
         print("Invalid request")
         main()
@@ -18,11 +23,18 @@ def load(filename):
     with open(filename, 'r') as f:
         return json.load(f)
 
+def write(filename, data):
+
+    with open(filename, 'w') as f:
+        json.dump(data, f)
+
 def find():
 
     d = load("servdata/population.json")
+
     population = d["population"]
     original_population = d["original_population"]
+    sus = d["sus"]
 
     c1 = input("what do you want to find? (Or B to go back)")
     if c1 == "population":
@@ -31,5 +43,10 @@ def find():
     elif c1 == "original population":
         print(original_population)
         find()
+    elif c1 == "sus":
+        sus += 1
+        print(f"You are X{sus} sus")
+        d["sus"] = sus
+        write("servdata/population.json", d)
     elif c1 == "b":
         main()
